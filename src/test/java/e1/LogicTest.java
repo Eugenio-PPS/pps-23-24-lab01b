5b97df830b45b01d116e5d7efae3b29f2de35de2
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2024-03-04 11:35:36 +0100

    Test everything except hit() because it uses random data

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,68 @@
 package e1;
 import org.junit.jupiter.api.*;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class LogicTest {
+    private Optional<Pair<Integer, Integer>> getKnightPosition(Logics board, int size) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                if(board.hasKnight(i, j)) {
+                    return Optional.of(new Pair<>(i, j));
+                }
+            }
+        }
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+        return Optional.empty();
+    }
+    private Optional<Pair<Integer, Integer>> getPawnPosition(Logics board, int size) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                if(board.hasPawn(i, j)) {
+                    return Optional.of(new Pair<>(i, j));
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Test
+    public void cannotCreateChessboardWithNegativeSize() {
+        assertThrows(IllegalArgumentException.class, () -> new LogicsImpl(-1));
+    }
+
+    @Test
+    public void cannotCreateChessboardWithZeroSize() {
+        assertThrows(IllegalArgumentException.class, () -> new LogicsImpl(0));
+    }
+    @Test
+    @Tag("To fix: should not throw StackOverflowError")
+    public void oneByOneBoardThrowsStackOverflowError() {
+        assertThrows(StackOverflowError.class, () -> new LogicsImpl(1));
+    }
+
+    @Test
+    public void chessboardHasBothKnightAndPawn() {
+        int boardSize = 2;
+        Logics board = new LogicsImpl(boardSize);
+
+        boolean pawnFound = this.getPawnPosition(board, boardSize).isPresent();
+        boolean knightFound = this.getKnightPosition(board, boardSize).isPresent();
+
+        assertTrue(pawnFound);
+        assertTrue(knightFound);
+    }
+
+    @Test
+    public void chessboardHasBothKnightAndPawnInDifferentPositions() {
+        int boardSize = 2;
+        Logics board = new LogicsImpl(boardSize);
+
+        Pair<Integer, Integer> pawn = this.getPawnPosition(board, boardSize).get();
+        Pair<Integer, Integer> knight = this.getKnightPosition(board, boardSize).get();
+
+        assertNotEquals(pawn, knight);
+    }
 }
